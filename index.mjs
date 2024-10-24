20f27cb00998a234716b92aa95d44e103e96ff79
xantasia <1028246@lcps.org>
2024-10-24 20:42:07 +0000

    week 3

--- index.mjs
+++ index.mjs
@@ -1,25 +1,21 @@
-import express from "express"
-import path from "node:path"
-import http from "node:http"
+import express from "express";
+import path from "node:path";
+import http from "node:http";
 
-const __dirname = path.resolve()
-const server = http.createServer()
-const app = express(server)
+const __dirname = path.resolve();
+const server = http.createServer();
+const app = express(server);
 
-app.use(
-    express.static(
-        path.join(__dirname, "bidi")
-    )
-)
+app.use(express.static(path.join(__dirname, "./bidi")));
 app.get("/", (req, res) => {
-    res.sendFile(__dirname, "./bidi/index.html")
-})
+    res.sendFile(path.join(__dirname, "./bidi/index.html"))
+});
 server.on("request", (req, res) => {
-    app(req, res)
-})
+  app(req, res);
+});
 server.on("listening", () => {
-    console.log("the server has been turned on")
-})
+  console.log("hello");
+});
 server.listen({
-    port: 9000
+    port: 4500
 })

c2449d158e75ae82c1378684ac7282d24a4de8c2
xantasia <1028246@lcps.org>
2024-11-07 21:43:59 +0000

    im so good

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ const app = express(server);
 
 app.use(express.static(path.join(__dirname, "./bidi")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./bidi/index.html"))
+  res.sendFile(path.join(__dirname, "./bidi/index.html"));
 });
 server.on("request", (req, res) => {
   app(req, res);
@@ -17,5 +17,5 @@ server.on("listening", () => {
   console.log("hello");
 });
 server.listen({
-    port: 4500
-})
+  port: 4500,
+});
